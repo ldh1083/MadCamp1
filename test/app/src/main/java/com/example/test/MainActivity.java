f46004eab26b60d2c39e4b6ef9da5609eac84b86
ldh <ldhldh9914@gmail.com>
2020-12-30 15:49:21 +0900

    try save new

--- test/app/src/main/java/com/example/test/MainActivity.java
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -1,9 +1,20 @@
 package com.example.test;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.test.ui.main.Phonenumber;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.test.ui.main.SectionsPagerAdapter;
@@ -12,13 +23,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
     public static ArrayList<Phonenumber> phonenumbers = new ArrayList<>();
 
+    private EditText nameEditText = null;
+    private EditText numberEditText = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,9 +63,9 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
 
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
-        tabs.addTab(tabs.newTab().setText("Contacts"));
-        tabs.addTab(tabs.newTab().setText("Gallery"));
-        tabs.addTab(tabs.newTab().setText("Free"));
+        tabs.addTab(tabs.newTab().setText("전화번호부"));
+        tabs.addTab(tabs.newTab().setText("갤러리"));
+        tabs.addTab(tabs.newTab().setText("자유"));
         tabs.setTabGravity(TabLayout.GRAVITY_FILL);
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
@@ -68,6 +81,96 @@ public class MainActivity extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.show();
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0) {
+                    fab.setImageResource(android.R.drawable.ic_input_add);
+                    fab.show();
+                } else {
+                    fab.hide();
+                }
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context mContext = getApplicationContext();
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+                View layout = inflater.inflate(R.layout.save_phonenumber,(ViewGroup) findViewById(R.id.layout_root));
+                AlertDialog.Builder aDialog = new AlertDialog.Builder(MainActivity.this);
+                aDialog.setTitle("New");
+                aDialog.setView(layout);
+                nameEditText = layout.findViewById(R.id.name);
+                numberEditText = layout.findViewById(R.id.number);
+                aDialog.setPositiveButton("저장", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newName = nameEditText.getText().toString();
+                        String newNunmber = numberEditText.getText().toString();
+                        phonenumbers.add(new Phonenumber(newName, newNunmber));
+                        setStringArrayPref("contact", phonenumbers);
+                        JSONObject jsonObject = new JSONObject();
+                        try{
+                            jsonObject.put("name", newName);
+                            jsonObject.put("number", newNunmber);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                aDialog.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                AlertDialog ad = aDialog.create();
+                ad.show();
+            }
+        });
+    }
+    public void setStringArrayPref(String key, ArrayList<Phonenumber> values) {
+        SharedPreferences prefs = getSharedPreferences("contact", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+        editor.commit();
+        System.out.println("hello");
+    }
+
+    private ArrayList<String> getStringArrayPref(Context context, String key) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = prefs.getString(key, null);
+        ArrayList<String> urls = new ArrayList<String>();
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
     }
 
 }
--- test/app/src/main/java/com/example/test/ui/main/PhoneNumberFragment.java
+++ test/app/src/main/java/com/example/test/ui/main/PhoneNumberFragment.java
@@ -8,11 +8,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
+
 import android.widget.ListView;
 import java.util.ArrayList;
 
 import com.example.test.MainActivity;
 import com.example.test.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PhoneNumberFragment extends Fragment  {
     private static final String ARG_SECTION_NUMBER = "section_number";
@@ -31,6 +34,7 @@ public class PhoneNumberFragment extends Fragment  {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.phonenumber_fragment, container, false);
         listView = (ListView) view.findViewById(R.id.listview1);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
         return view;
     }
 

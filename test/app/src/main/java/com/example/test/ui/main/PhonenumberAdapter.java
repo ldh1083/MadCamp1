5fba0530aac2cbc6024082c82c067c9a9c53f60b
ldh <ldhldh9914@gmail.com>
2021-01-04 16:21:03 +0900

    done

--- test/app/src/main/java/com/example/test/ui/main/PhonenumberAdapter.java
+++ test/app/src/main/java/com/example/test/ui/main/PhonenumberAdapter.java
@@ -1,15 +1,23 @@
 package com.example.test.ui.main;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import com.example.test.R;
+
+import static com.example.test.MainActivity.phonenumbers;
 
 public class PhonenumberAdapter extends ArrayAdapter<Phonenumber> {
     private Context context;
@@ -30,6 +38,51 @@ public class PhonenumberAdapter extends ArrayAdapter<Phonenumber> {
         nameTextView.setText(phonenumbers.get(position).getName());
         final TextView numberTextView = (TextView) rowView.findViewById(R.id.number);
         numberTextView.setText(phonenumbers.get(position).getNumber());
+
+        ImageView iv_contact = (ImageView)rowView.findViewById(R.id.image_contact);
+
+        ContentResolver cr = context.getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+            Uri u= null;
+            while (cur != null && cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (name.equalsIgnoreCase(phonenumbers.get(position).getName()))
+                {
+                    try {
+                        Cursor cur1 = cr.query(
+                                ContactsContract.Data.CONTENT_URI,
+                                null,
+                                ContactsContract.Data.CONTACT_ID + "=" + id + " AND "
+                                        + ContactsContract.Data.MIMETYPE + "='"
+                                        + ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE + "'", null,
+                                null);
+                        if (cur1 != null) {
+                            if (!cur1.moveToFirst()) {
+                                break; // no photo
+                            }
+                        } else {
+                            break; // error in cursor process
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                    Uri person = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, Long
+                            .parseLong(id));
+                    u = Uri.withAppendedPath(person, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+                    break;
+                }
+            }
+
+            if (u != null) {
+                iv_contact.setImageURI(u);
+            }
+        }
+
         // View에 Data 세팅
         return rowView;
     }
